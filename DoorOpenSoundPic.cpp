f700ed28d8aa6ea0d40c3fd81a0df5bdc9949b19
M Zubair <hazzelwood786@gmail>
2021-11-11 02:11:36 +0500

    File added.

---
+++ DoorOpenSoundPic.cpp
@@ -0,0 +1,30 @@
+#include <reg51.h>
+
+void MsDelay (unsigned int);
+
+sbit Dsensor = P1^1;
+sbit Dbuzzer = P1^7;
+
+void main(main)
+{
+	Dsensor = 1;
+	
+	while(1)
+	{
+		while(Dsensor == 1)
+		{
+			Dbuzzer = 0;
+			MsDelay(100);
+			Dbuzzer = 1;
+			MsDelay(100);
+		}
+	}
+}
+
+void MsDelay(int Z)
+{
+		for(int i=0; i<z; i++)
+		{
+			i;
+		}
+}
